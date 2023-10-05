cf82e2d8f61afa923fb3360b60d7513d01e917ee
Liviru Abeygunawardena <liviru2003@gmail.com>
2023-10-05 15:06:57 -0400

    Interface window made

--- DCM/main.py
+++ DCM/main.py
@@ -3,3 +3,20 @@ import tkinter.messagebox as messagebox
 
 
 
+class SimpleLoginApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Pacemaker Interface")
+        self.geometry("600x600")
+
+        # Create labels
+        self.welcome_label = tk.Label(self, text="Welcome!", font=("Arial", 14))
+        self.welcome_label.pack(pady=10)
+
+
+
+
+if __name__ == "__main__":
+    app = SimpleLoginApp()
+    app.mainloop()

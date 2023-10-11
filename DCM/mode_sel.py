4d2064137b423482f78c41e2765a1a41124371a7
Bhavya Shah <woburnp@gmail.com>
2023-10-11 03:24:15 -0400

    Added the parameter entry GUI for each mode

--- DCM/mode_sel.py
+++ DCM/mode_sel.py
@@ -2,7 +2,6 @@ import tkinter as tk
 import json
 import tkinter.messagebox as messagebox
 
-
 class ModeSel:
     def __init__(self, root, main_app):
         self.root = root
@@ -14,26 +13,38 @@ class ModeSel:
         modes = ["AOO", "VOO", "AAI", "VVI"]
         mode_dropdown = tk.OptionMenu(root, self.mode_var, *modes)
         mode_dropdown.place(x=200, y=30)
-        next_button = tk.Button(root, text="Next", command = lambda:  self.render(self.mode_var.get()))
+        next_button = tk.Button(root, text="Next", command=lambda: self.render(self.mode_var.get()))
         next_button.place(x=400, y=30)
 
         self.back_button = tk.Button(root, width='10', border=2, text="Log Out", font=("Inter", 10, 'bold'), fg='white', bg='red', cursor='hand2', command=lambda: self.main.route(self.main.login_frame))
         self.back_button.place(x=1000, y=20)
 
+        # Create a dictionary to map modes to their parameters
+        self.mode_parameters = {
+            "AOO": ["Upper Rate Limit", "Lower Rate Limit", "Atrial Amplitude", "Atrial Pulse Width"],
+            "VOO": ["Upper Rate Limit", "Lower Rate Limit", "Ventricular Amplitude", "Ventricular Pulse Width"],
+            "AAI": ["Upper Rate Limit", "Lower Rate Limit", "Atrial Amplitude", "Atrial Pulse Width", "ARP"],
+            "VVI": ["Upper Rate Limit", "Lower Rate Limit", "Ventricular Amplitude", "Ventricular Pulse Width", "VRP"]
+        }
+
+        self.current_widgets = []  # To store current widgets for mode parameters
+
     def render(self, mode):
-        if mode == "AOO":
-            print("Mode is AOO")
-        elif mode == "VOO":
-            print("Mode is VOO")
-        elif mode == "AAI":
-            print("Mode is AAI")
-        elif mode == "VVI":
-            print("Mode is VVI")
+        # Clear existing widgets
+        for widget in self.current_widgets:
+            widget.destroy()
+        
+        if mode in self.mode_parameters:
+            parameters = self.mode_parameters[mode]
+            row = 70  # Adjust the starting Y position
+            for param in parameters:
+                label = tk.Label(self.root, text=param)
+                label.place(x=100, y=row)
+                entry = tk.Entry(self.root)
+                entry.place(x=250, y=row)
+                self.current_widgets.extend([label, entry])
+                row += 30  # Adjust the vertical spacing
+
         else:
             pass
 
-
-
-
-
-        

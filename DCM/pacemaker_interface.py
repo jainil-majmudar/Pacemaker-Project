09e46674a1c550ca9c85836de35f483aee4974aa
Bhavya Shah <woburnp@gmail.com>
2023-10-12 16:36:44 -0400

    Previous pacemaker works even if shut down now

--- DCM/pacemaker_interface.py
+++ DCM/pacemaker_interface.py
@@ -1,16 +1,15 @@
-# pacemaker_interface.py
 import tkinter as tk
 import json
 import tkinter.messagebox as messagebox
-
 
 class PacemakerInterface:
     def __init__(self, root, main_app):
         self.root = root
         self.main = main_app
 
-        self.prev_pacemaker = "None"  # Make it an instance variable
-        self.previous_pacemaker_label = tk.Label(root, text="Previous Pacemaker Connected: " + "None")
+        # Load the previously selected pacemaker from a file
+        self.prev_pacemaker = self.load_previous_pacemaker()
+        self.previous_pacemaker_label = tk.Label(root, text=f"Previous Pacemaker Connected: {self.prev_pacemaker}")
         self.previous_pacemaker_label.place(x=100, y=50)
 
         # Create and configure the pacemaker interface widgets
@@ -19,9 +18,9 @@ class PacemakerInterface:
         self.pacemaker_entry = tk.Entry(root, width=46)
         self.pacemaker_entry.place(x=300, y=100)
 
-        self.connection_label = tk.Label(root, text="Communication Established: " + "No")
-        self.connection_label.place(x=100,y=500)
-        
+        self.connection_label = tk.Label(root, text="Communication Established: No")
+        self.connection_label.place(x=100, y=500)
+
         self.submit_button = tk.Button(root, text="Submit", command=lambda: self.submit(self.pacemaker_entry.get()))
         self.submit_button.place(x=600, y=100)
 
@@ -31,19 +30,28 @@ class PacemakerInterface:
         # Place the widgets in the interface
         self.root.pack()
 
-    def submit(self,entry):
-        
-        if(self.prev_pacemaker!=entry):
+    def load_previous_pacemaker(self):
+        try:
+            with open("DCM/DataStorage/previous_pacemaker.json", "r") as file:
+                data = json.load(file)
+                return data.get("previous_pacemaker", "None")
+        except FileNotFoundError:
+            return "None"
+
+    def save_previous_pacemaker(self, pacemaker):
+        data = {"previous_pacemaker": pacemaker}
+        with open("DCM/DataStorage/previous_pacemaker.json", "w") as file:
+            json.dump(data, file)
+
+    def submit(self, entry):
+        if self.prev_pacemaker != entry:
             result = messagebox.askquestion("Note!", "The pacemaker entered is different than the last one connected. Is this okay?")
-            if(result == 'yes'):
+            if result == 'yes':
                 self.prev_pacemaker = entry
-                self.previous_pacemaker_label.config(text="Previous Pacemaker Connected: " + entry)
-                messagebox.showinfo("Note!","Connected successfully")
+                self.previous_pacemaker_label.config(text=f"Previous Pacemaker Connected: {entry}")
+                self.save_previous_pacemaker(entry)  # Save the new pacemaker
+                messagebox.showinfo("Note!", "Connected successfully")
                 self.main.route(self.main.mode_sel)
         else:
-            messagebox.showinfo("Note!","Connected successfully")
+            messagebox.showinfo("Note!", "Connected successfully")
             self.main.route(self.main.mode_sel)
-   
-
-        
-
